2d4b1775e8093ab31e2802fbb85757bba3845871
Efe Canbolat <efecanbolat34@gmail.com>
2023-04-02 01:46:31 +0300

    Delete submit_mail.php

--- submit_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $to = "e.canbolatlnx@gmail.com";
-    $subject = "Yeni İletişim Mesajı";
-    $body = "İsim: $name\nE-posta: $email\n\nMesaj:\n$message";
-    mail($to, $subject, $body);
-?>
